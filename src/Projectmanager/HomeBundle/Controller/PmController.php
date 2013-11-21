e0e90e25161a8d1efb86226a77b21bb3e8b8ed51
Anthony <anthonypa.orsat@gmail.com>
2013-11-21 16:36:23 +0100

    projet de base

---
+++ src/Projectmanager/HomeBundle/Controller/PmController.php
@@ -0,0 +1,21 @@
+<?php
+
+// src/Sdz/BlogBundle/Controller/PmController.php
+
+namespace Projectmanager\HomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class PmController extends Controller
+{
+  public function indexAction()
+  {
+    	return $this->render('ProjectmanagerHomeBundle:Pm:index.html.twig');
+  }
+	public function seeProjectAction($id)
+  {
+   	
+	return $this->render('ProjectmanagerHomeBundle:Pm:seeProject.html.twig', array('id' => $id));
+  }
+}
---
+++ src/Projectmanager/HomeBundle/ProjectmanagerHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Projectmanager\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProjectmanagerHomeBundle extends Bundle
+{
+}
